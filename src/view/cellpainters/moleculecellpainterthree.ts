b731e9fd1bc70366d3fdd66a86db73b7d386204c
Fredrik Kemling <kemling@gmail.com>
2022-05-25 23:36:38 +0200

    molecule 3

--- src/view/cellpainters/moleculecellpainterthree.ts
+++ src/view/cellpainters/moleculecellpainterthree.ts
@@ -1,3 +1,4 @@
+import { Cell } from '../../model/cell';
 import { Grid } from '../../model/grid';
 import { CanvasPainter } from './canvaspainter';
 import { CellPainter } from './cellpainter';
@@ -7,6 +8,54 @@ export class MoleculeCellPainterThree extends CanvasPainter implements CellPaint
     plotCells(grid: Grid): void {
 
         //dela upp cellerna i kluster
+        grid.clusters.forEach(cluster => {
+            const cellsToVisit: number = cluster.length;
+            const visitedCells: Set<Cell> = new Set();
+            visitedCells.add(cluster[0]);
+            const visitTrail: Cell[] = [cluster[0]];
+
+            while (cellsToVisit > visitedCells.size) {
+                while (cellHasNoUnvisitedNeigbours(currentCell()) && visitedTrailNotEmpty()) {
+                    stepBackwards();
+                }
+                const randomNeighbour: Cell = randomUnvisitedNeighbour(currentCell());
+                this.paintThinLineBetweenCells(currentCell(), randomNeighbour, 2.5);
+                visitRandomNeighbour(randomNeighbour);
+            }
+
+
+            function currentCell(): Cell {
+                const currentCell: Cell = visitTrail[visitTrail.length - 1];
+                return currentCell;
+            }
+
+            function visitedTrailNotEmpty(): boolean {
+                return visitTrail.length != 0;
+            }
+
+            function cellHasNoUnvisitedNeigbours(cell: Cell): boolean {
+                return cell.livingNeighbours.every(neigbour => visitedCells.has(neigbour));
+            }
+
+            function stepBackwards(): void {
+                visitTrail.pop();
+            }
+
+            function randomUnvisitedNeighbour(cell: Cell): Cell {
+                const unvisitedNeighbours: Cell[] = cell.livingNeighbours
+                    .filter(neighbour => !visitedCells.has(neighbour));
+                const randomUnvisitedNeighbour: Cell =
+                    unvisitedNeighbours[Math.floor(Math.random() * unvisitedNeighbours.length)];
+                return randomUnvisitedNeighbour;
+            }
+
+            function visitRandomNeighbour(cell: Cell): void {
+                visitedCells.add(cell);
+                visitTrail.push(cell);
+            }
+
+
+        });
 
 
 
