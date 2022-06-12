b0a86cf9c4e049ad200b5b6d2956e53285a189ed
Fredrik Kemling <kemling@gmail.com>
2022-06-12 23:13:37 +0200

    reworking molecule

--- src/view/cellpainters/moleculepainter.ts
+++ src/view/cellpainters/moleculepainter.ts
@@ -26,12 +26,22 @@ export class MoleculePainter extends CanvasPainter implements CellPainter {
             });
 
         this.cellsWithOneLivingOrthogonalNeihbours(livingCells).forEach(cell => {
-            this.cellsWithOneLivingOrthogonalNeihbours(cell.livingDiagonalNeighbours)
-                .forEach(neighbour => this.paintThinLineBetweenCells(cell, neighbour));
+            cell.livingDiagonalNeighbours
+                .forEach(neighbour => {
+                    if (this.cellsHaveNoCommonLivingOrthogonalNeighbours(cell,neighbour)) {
+                        this.paintThinLineBetweenCells(cell, neighbour);
+                    }
+                });
         });
 
 
         this.paintCellsAsHollowDots(livingCells, 'rgba(0,0,0,1)', 'rgba(128,128,0,1)');
+    }
+
+    private cellsHaveNoCommonLivingOrthogonalNeighbours(cell1: Cell, cell2: Cell): boolean {
+        const commonLivingOrthogonalCells: Cell[] = cell1.livingOrthogonalNeighbours
+            .filter(c1 => cell2.livingOrthogonalNeighbours.some(c2 => c1 == c2));
+        return commonLivingOrthogonalCells.length == 0;
     }
 
     private cellsWithNoLivingOrthogonalNeihbours(cells: Cell[]): Cell[] {
