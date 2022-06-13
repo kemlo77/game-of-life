92972463f8dcdb683ef7ce6a44193d3ad82da699
Fredrik Kemling <kemling@gmail.com>
2022-06-13 22:57:28 +0200

    modifications to molecule

--- src/view/cellpainters/moleculepainter.ts
+++ src/view/cellpainters/moleculepainter.ts
@@ -35,7 +35,69 @@ export class MoleculePainter extends CanvasPainter implements CellPainter {
         });
 
 
-        this.paintCellsAsHollowDots(livingCells, 'rgba(0,0,0,1)', 'rgba(128,128,0,1)');
+        this.paintCellsAsHollowDots(livingCells, 'rgba(0,0,0,1)', 'rgba(128,128,128,1)');
+        this.paintCellsAsHollowDots(
+            this.cellsWithNoLivingNeighbours(livingCells),
+            'rgba(0,0,0,1)',
+            'rgba(255,255,0,1)'
+        );
+        this.paintCellsAsHollowDots(
+            this.cellsWithOneLivingOrthogonalNeighbour(livingCells),
+            'rgba(0,0,0,1)',
+            'rgba(0,255,0,1)'
+        );
+
+
+        this.cellsWithOneLivingOrthogonalNeihbours(livingCells).forEach(cell => {
+            cell.livingDiagonalNeighbours
+                .forEach(neighbour => {
+                    if (this.cellsHaveNoCommonLivingOrthogonalNeighbours(cell,neighbour)) {
+                        this.paintCellsAsHollowDots([cell, neighbour],'rgba(0,0,0,1)','rgba(128,128,128,1)');
+                    }
+                });
+        });
+
+        this.paintCellsAsHollowDots(
+            this.cellsWithOneLivingDiagonalNeighbour(livingCells),
+            'rgba(0,0,0,1)',
+            'rgba(100,100,255,1)'
+        );
+
+        this.paintCellsAsHollowDots(
+            this.cellsWithTwoDiagonalNeighboursNotOnALine(livingCells),
+            'rgba(0,0,0,1)',
+            'rgba(255,127,0,1)'
+        );
+
+    }
+
+
+    private cellsWithTwoDiagonalNeighboursNotOnALine(cells: Cell[]): Cell[] {
+        return cells
+            .filter(cell => cell.livingNeighbours.length == 2)
+            .filter(cell => cell.livingDiagonalNeighbours.length == 2)
+            .filter(cell => {
+                const neighbours: Cell[] = cell.livingDiagonalNeighbours;
+                const n1: Cell = neighbours[0];
+                const n2: Cell = neighbours[1];
+                return n1.columnIndex == n2.columnIndex || n1.rowIndex == n2.rowIndex;
+            });
+    }
+
+    private cellsWithNoLivingNeighbours(cells: Cell[]): Cell[] {
+        return cells.filter(cell => cell.livingNeighbours.length == 0);
+    }
+
+    private cellsWithOneLivingOrthogonalNeighbour(cells: Cell[]): Cell[] {
+        return cells
+            //.filter(cell => cell.livingNeighbours.length == 1)
+            .filter(cell => cell.livingOrthogonalNeighbours.length == 1);
+    }
+
+    private cellsWithOneLivingDiagonalNeighbour(cells: Cell[]): Cell[] {
+        return cells
+            .filter(cell => cell.livingNeighbours.length == 1)
+            .filter(cell => cell.livingDiagonalNeighbours.length == 1);
     }
 
     private cellsHaveNoCommonLivingOrthogonalNeighbours(cell1: Cell, cell2: Cell): boolean {
